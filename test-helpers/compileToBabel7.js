3afc2368efa7fa106bba0318716cbee631f5ec43
Christopher Pappas <damassi.pappas@gmail.com>
2018-02-08 17:46:13 -0800

    Tests almost pass

--- test-helpers/compileToBabel7.js
+++ test-helpers/compileToBabel7.js
@@ -1,61 +1,64 @@
 const babel = require("@babel/core");
+const expect = require("expect.js");
 const fs = require("fs");
 const path = require("path");
 const babelPluginRewire = require("../lib/babel-plugin-rewire.js");
+const prettier = require("prettier");
+const UglifyJS = require("uglify-es");
 
 var featuresToTest = [
-  "babelissue1315",
-  "issue16",
-  "forOf",
-  "commonJSExportOnly",
-  "defaultImport",
-  "defaultExport",
-  "defaultExportImport",
-  "defaultExportWithClass",
-  "defaultExportWithNamedFunction",
-  "defaultExportWithObject",
-  "issuePathReplaceWith",
-  "importWithReactClass",
-  "jsxSupport",
-  "jsxWithComponentImport",
-  "moduleExports",
-  "multipleImports",
-  "multipleImportsWithAliases",
-  "namedFunctionExport",
-  "namedFunctionImport",
-  "namedVariableExport",
-  "noDefaultExport",
-  "passThrough",
-  "primitiveExportWithNamedFunctionExport",
-  "wildcardImport",
-  "wildcardExport",
-  "namedWildcardExport",
-  "recursiveRewireCall",
-  "requireExports",
-  "requireMultiExports",
-  "switch",
-  "topLevelVar",
-  "functionRewireScope",
-  "issue69",
-  "issue71-tdz",
-  "issue71-tdz-index",
-  "flowTypeExport",
-  "flowTypeImport",
-  "updateOperations",
-  "assignmentOperations",
-  "rewiringOfReactComponents",
-  "rewiringOfSimpleFunctionalComponents",
-  "issue121",
-  "issue133",
-  "issue136",
-  "issue152",
-  "issue155"
+  "babelissue1315"
+  // "issue16"
+  // "forOf"
+  // "commonJSExportOnly",
+  // "defaultImport",
+  // "defaultExport",
+  // "defaultExportImport",
+  // "defaultExportWithClass",
+  // "defaultExportWithNamedFunction",
+  // "defaultExportWithObject",
+  // "issuePathReplaceWith",
+  // "importWithReactClass",
+  // "jsxSupport",
+  // "jsxWithComponentImport",
+  // "moduleExports",
+  // "multipleImports",
+  // "multipleImportsWithAliases",
+  // "namedFunctionExport",
+  // "namedFunctionImport",
+  // "namedVariableExport",
+  // "noDefaultExport",
+  // "passThrough",
+  // "primitiveExportWithNamedFunctionExport",
+  // "wildcardImport",
+  // "wildcardExport",
+  // "namedWildcardExport",
+  // "recursiveRewireCall",
+  // "requireExports",
+  // "requireMultiExports",
+  // "switch",
+  // "topLevelVar",
+  // "functionRewireScope",
+  // "issue69",
+  // "issue71-tdz",
+  // "issue71-tdz-index",
+  // "flowTypeExport",
+  // "flowTypeImport",
+  // "updateOperations",
+  // "assignmentOperations",
+  // "rewiringOfReactComponents",
+  // "rewiringOfSimpleFunctionalComponents",
+  // "issue121",
+  // "issue133",
+  // "issue136",
+  // "issue152",
+  // "issue155"
 ];
 
 const babelOptions = {
   presets: ["@babel/react"],
   plugins: [
-    // babelPluginRewire,
+    babelPluginRewire,
     "@babel/syntax-flow",
     "@babel/proposal-export-namespace-from"
   ]
@@ -70,12 +73,25 @@ function compileTestInput(testName) {
     testName
   );
   var input = fs.readFileSync(path.resolve(directory, "input.js"), "utf-8");
-  console.log("\n------------", path.resolve(directory, "input.js"), "\n");
-  // var expected = fs.readFileSync(path.resolve(directory, 'expected.js'), 'utf-8');
+
+  // console.log("\n------------", path.resolve(directory, "input.js"), "\n");
+
+  var expected = fs.readFileSync(
+    path.resolve(directory, "expected.js"),
+    "utf-8"
+  );
 
   try {
-    var transformationOutput = babel.transform(input, babelOptions).code;
+    var transformationOutput = UglifyJS.minify(
+      prettier.format(babel.transform(input, babelOptions).code),
+      { mangle: false }
+    );
+
     console.log(transformationOutput);
+    console.log("\n------------", path.resolve(directory, "input.js"), "\n");
+    console.log(UglifyJS.minify(expected, { mangle: false }));
+    // console.log(transformationOutput);
+    // expect(transformationOutput.trim()).to.be(expected.trim());
   } catch (error) {
     expect().fail("Transformation failed: \n" + error.stack);
   }
--- test/BabelRewirePluginTransformTest.js
+++ test/BabelRewirePluginTransformTest.js
@@ -1,23 +1,23 @@
-var babel = require("@babel/core");
-var path = require("path");
-var fs = require("fs");
-var os = require("os");
-var expect = require("expect.js");
-var babelPluginRewire = require("../lib/babel-plugin-rewire.js"); //  require('../test-helpers/getBabelPluginRewire.js');
+const babel = require("@babel/core");
+const path = require("path");
+const fs = require("fs");
+const os = require("os");
+const expect = require("expect.js");
+const prettier = require("prettier");
+const UglifyJS = require("uglify-es");
+const babelPluginRewire = require("../lib/babel-plugin-rewire.js"); //  require('../test-helpers/getBabelPluginRewire.js');
 
-describe("BabelRewirePluginTest", function() {
-  var babelTranslationOptions = {
+describe("BabelRewirePluginTest", () => {
+  const babelTranslationOptions = {
     presets: ["@babel/react"],
     plugins: [
       babelPluginRewire,
-      // "syntax-async-functions",
       "@babel/syntax-flow",
       "@babel/proposal-export-namespace-from"
-      // "transform-export-extensions"
     ]
   };
 
-  var babelTranslationOptionsIgnoredIdentifiers = {
+  const babelTranslationOptionsIgnoredIdentifiers = {
     presets: ["@babel/react"],
     plugins: [
       [
@@ -26,18 +26,15 @@ describe("BabelRewirePluginTest", function() {
           ignoredIdentifiers: ["ignoredIdentifier1", "ignoredIdentifier2"]
         }
       ],
-      // "syntax-async-functions",
       "@babel/syntax-flow",
       "@babel/proposal-export-namespace-from"
-      // "transform-export-extensions"
     ]
   };
 
-  var babelTranslationOptionsAllEnabled = {
-    presets: ["@babel/es2015", "@babel/react"], //,
+  const babelTranslationOptionsAllEnabled = {
+    presets: ["@babel/es2015", "@babel/react", "@babel/stage-0"],
     plugins: [
       babelPluginRewire,
-      // "syntax-async-functions",
       "@babel/transform-runtime",
       "@babel/transform-block-scoping",
       "@babel/transform-template-literals",
@@ -47,8 +44,16 @@ describe("BabelRewirePluginTest", function() {
     ]
   };
 
+  function validate(input, output) {
+    const condense = expanded => {
+      return UglifyJS.minify(expanded, { mangle: false }).code;
+    };
+
+    expect(condense(input)).to.be(condense(output));
+  }
+
   function testTranslation(testName, options) {
-    var directory = path.resolve(
+    const directory = path.resolve(
       __dirname,
       "..",
       "fixtures",
@@ -56,20 +61,23 @@ describe("BabelRewirePluginTest", function() {
       testName
     );
 
-    var input = fs.readFileSync(path.resolve(directory, "input.js"), "utf-8");
-    var expected = fs.readFileSync(
+    const input = fs.readFileSync(path.resolve(directory, "input.js"), "utf-8");
+    const expected = fs.readFileSync(
       path.resolve(directory, "expected.js"),
       "utf-8"
     );
 
     try {
-      var transformationOutput = babel.transform(input, options).code;
+      var transformationOutput = prettier.format(
+        babel.transform(input, options).code
+      );
     } catch (error) {
       expect().fail("Transformation failed: \n" + error.stack);
     }
 
-    var tempDir = path.resolve(os.tmpdir(), "babel-plugin-rewire");
+    const tempDir = path.resolve(os.tmpdir(), "babel-plugin-rewire");
     console.log("TempDir: " + tempDir);
+
     try {
       fs.mkdirSync(tempDir);
     } catch (error) {}
@@ -79,50 +87,58 @@ describe("BabelRewirePluginTest", function() {
       transformationOutput,
       "utf-8"
     );
+
     //fs.writeFileSync(path.resolve(directory, 'expected.js'), transformationOutput, 'utf-8');
 
     if (expected.trim() != transformationOutput.trim()) {
       console.log(transformationOutput);
     }
-    expect(transformationOutput.trim()).to.be(expected.trim());
+
+    validate(transformationOutput, expected);
   }
 
   function testSuccessfulTranslation(testName, additionalOptions) {
-    var directory = path.resolve(
+    const directory = path.resolve(
       __dirname,
       "..",
       "fixtures",
       "transformation",
       testName
     );
-    var input = fs.readFileSync(path.resolve(directory, "input.js"), "utf-8");
 
-    var transformationResult = babel.transform(
-      input,
-      combineOptions(babelTranslationOptionsAllEnabled, additionalOptions)
+    const input = fs.readFileSync(path.resolve(directory, "input.js"), "utf-8");
+
+    const transformationResult = prettier.format(
+      babel.transform(
+        input,
+        combineOptions(babelTranslationOptionsAllEnabled, additionalOptions)
+      )
     );
   }
 
   function testIgnoredIdentifiersTranslation(testName) {
-    var directory = path.resolve(
+    const directory = path.resolve(
       __dirname,
       "..",
       "fixtures",
       "transformation",
       testName
     );
-    var input = fs.readFileSync(path.resolve(directory, "input.js"), "utf-8");
 
-    var transformationResult = babel.transform(
-      input,
-      babelTranslationOptionsAllEnabledIgnoredIdentifiers
+    const input = fs.readFileSync(path.resolve(directory, "input.js"), "utf-8");
+
+    const transformationResult = prettier.format(
+      babel.transform(
+        input,
+        babelTranslationOptionsAllEnabledIgnoredIdentifiers
+      )
     );
   }
 
   function combineOptions(baseOptions, additionalOptions) {
-    var additionalPresets =
+    const additionalPresets =
       (additionalOptions && additionalOptions.presets) || [];
-    var additionalPlugins =
+    const additionalPlugins =
       (additionalOptions && additionalOptions.plugins) || [];
 
     return {
@@ -131,84 +147,73 @@ describe("BabelRewirePluginTest", function() {
     };
   }
 
-  var featuresToTest = [
-    "babelissue1315"
-    // 'issue16',
-    // 'forOf',
-    // 'commonJSExportOnly',
-    // 'defaultImport',
-    // 'defaultExport',
-    // 'defaultExportImport',
-    // 'defaultExportWithClass',
-    // 'defaultExportWithNamedFunction',
-    // 'defaultExportWithObject',
-    // 'issuePathReplaceWith',
-    // 'importWithReactClass',
-    // 'jsxSupport',
-    // 'jsxWithComponentImport',
-    // 'moduleExports',
-    // 'multipleImports',
-    // 'multipleImportsWithAliases',
-    // 'namedFunctionExport',
-    // 'namedFunctionImport',
-    // 'namedVariableExport',
-    // 'noDefaultExport',
-    // 'passThrough',
-    // 'primitiveExportWithNamedFunctionExport',
-    // 'wildcardImport',
-    // 'wildcardExport',
-    // 'namedWildcardExport',
-    // 'recursiveRewireCall',
-    // 'requireExports',
-    // 'requireMultiExports',
-    // 'switch',
-    // 'topLevelVar',
-    // 'functionRewireScope',
-    // 'issue69',
-    // 'issue71-tdz',
-    // 'issue71-tdz-index',
-    // 'flowTypeExport',
-    // 'flowTypeImport',
-    // 'updateOperations',
-    // 'assignmentOperations',
-    // 'rewiringOfReactComponents',
-    // 'rewiringOfSimpleFunctionalComponents',
-    // 'issue121',
-    // 'issue133',
-    // 'issue136',
-    // 'issue152',
-    // 'issue155'
+  const featuresToTest = [
+    "babelissue1315",
+    "issue16",
+    "forOf",
+    "commonJSExportOnly",
+    "defaultImport",
+    "defaultExport",
+    "defaultExportImport",
+    "defaultExportWithClass",
+    "defaultExportWithNamedFunction",
+    "defaultExportWithObject",
+    "issuePathReplaceWith",
+    "importWithReactClass",
+    "jsxSupport",
+    "jsxWithComponentImport",
+    "moduleExports",
+    "multipleImports",
+    "multipleImportsWithAliases",
+    "namedFunctionExport",
+    "namedFunctionImport",
+    "namedVariableExport",
+    "noDefaultExport",
+    "passThrough",
+    "primitiveExportWithNamedFunctionExport",
+    "wildcardImport",
+    "wildcardExport",
+    "namedWildcardExport",
+    "recursiveRewireCall",
+    "requireExports",
+    "requireMultiExports",
+    "switch",
+    "topLevelVar",
+    "functionRewireScope",
+    "issue69",
+    "issue71-tdz",
+    "issue71-tdz-index",
+    // "flowTypeExport",
+    // "flowTypeImport",
+    "updateOperations",
+    "assignmentOperations",
+    "rewiringOfReactComponents",
+    "rewiringOfSimpleFunctionalComponents",
+    "issue121",
+    "issue133",
+    "issue136",
+    "issue152",
+    "issue155"
   ];
 
-  var stage0FeaturesToTests = ["issue164"];
+  const stage0FeaturesToTests = ["issue164"];
+  const ignoredIdentifiers = ["ignoredIdentifiers"];
 
-  var ignoredIdentifiers = ["ignoredIdentifiers"];
-
-  featuresToTest.forEach(function(feature) {
-    it(
-      "test babel-plugin-rewire for " + feature,
-      testTranslation.bind(null, feature, babelTranslationOptions)
-    );
+  featuresToTest.forEach(feature => {
+    it(`test babel-plugin-rewire for ${feature}`, () => {
+      testTranslation(feature, babelTranslationOptions);
+    });
   });
 
-  // featuresToTest.forEach(function(feature) {
-  // 	it('test translation babel-plugin-rewire with ignored identifiers for ' + feature, testTranslation.bind(null, feature, babelTranslationOptions));
-  // });
+  featuresToTest.forEach(feature => {
+    it(`test translation babel-plugin-rewire with ignored identifiers for ${feature}`, () => {
+      testTranslation(feature, babelTranslationOptions);
+    });
+  });
 
-  // ignoredIdentifiers.forEach(function(feature) {
-  // 	it('test translation babel-plugin-rewire with ignored identifiers for ' + feature, testTranslation.bind(null, feature, babelTranslationOptionsIgnoredIdentifiers));
-  // });
-
-  // featuresToTest.forEach(function(feature) {
-  // 	it('test successful translation babel-plugin-rewire for ' + feature, testSuccessfulTranslation.bind(null, feature, {}));
-  // });
-
-  // stage0FeaturesToTests.forEach(function(feature) {
-  // 	var additionalOptions = {
-  // 		presets: ['@babel/stage-0'],
-  // 	};
-
-  // 	it('test successful translation babel-plugin-rewire for ' + feature, testSuccessfulTranslation.bind(null, feature, combineOptions(babelTranslationOptions, additionalOptions)));
-  // 	it('test translation babel-plugin-rewire for ' + feature, testSuccessfulTranslation.bind(null, feature, additionalOptions));
-  // });
+  ignoredIdentifiers.forEach(feature => {
+    it(`test translation babel-plugin-rewire with ignored identifiers for ${feature}`, () => {
+      testTranslation(feature, babelTranslationOptionsIgnoredIdentifiers);
+    });
+  });
 });
